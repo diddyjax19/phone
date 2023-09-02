327356b5a267f7ec5c2add06fe5e81a33201ddb2
diddyjax19 <47352532+diddyjax19@users.noreply.github.com>
2023-09-02 17:46:36 +0200

    changed the debug to false

--- phonestore/settings.py
+++ phonestore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nestit-d6952187268f.herokuapp.com', 'localhost','127.0.0.1']
 
